e935f0aeae4b7e093c96d8f4034a7da7e7b2defc
LucasGlave <lucasglave7@gmail.com>
2023-11-13 13:04:33 -0300

    creacion de carpetas

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./login.scss";
 import { Roboto } from "next/font/google";
+import styles from "../../app/login.module.scss";
 
 const Login = () => {
   return (
     <div className="container">
       <div className="header">
-        <h3>Reservar</h3>
-        <div>
+        <h3 style={{ color: "#a442f1" }}>Reservar</h3>
+        <div className="headerRight">
           <h3>Mis reservas</h3>
           <h3>Mi cuenta</h3>
         </div>
@@ -23,7 +24,12 @@ const Login = () => {
             <h2>Contraseña</h2>
             <input type="text" />
           </div>
+          <h4>¿Olvidaste tu contraseña?</h4>
+          <div className="form-group">
+            <button>Ingresar</button>
+          </div>
         </form>
+        <h4 style={{ marginTop: "4rem" }}>¿No tenés una cuenta? Registrate</h4>
       </div>
     </div>
   );
