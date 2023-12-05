2da67ae4ee24366e290f79bef207f1ee5e90ebeb
FabiolaLutrario <fabiolalutrario@gmail.com>
2023-12-04 23:09:46 -0300

    Agregando withCredentials:true al service de reasonCancellation

--- src/services/reasonCancellation.js
+++ src/services/reasonCancellation.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+axios.defaults.withCredentials = true;
 
 export const reasonCancellationServiceAll = () => {
-  return axios.get("http://localhost:5001/api/reason-cancellations")
-  .then((reasonCancellations)=>reasonCancellations.data)
+  return axios
+    .get("http://localhost:5001/api/reason-cancellations")
+    .then((reasonCancellations) => reasonCancellations.data);
 };
